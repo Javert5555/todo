7cdc51b0eb64e67c194d4f5b3d205ee9c0f1e558
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-08-27 17:00:29 +0300

    add eslint

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const getTaskListItem = (uuid, value) => `<li class="task_list__item task_list__
     <input
         type="text"
         name="task_text"
-        id="task_text"
+        id="task_text-${uuid}"
         data-task-uuid="${uuid}"
         value="${value}"
         class="task_list__item-text"
@@ -25,294 +25,275 @@ const getTaskListItem = (uuid, value) => `<li class="task_list__item task_list__
 </div>
 </li>`
 
-function getUuid () {
-    return self.crypto.randomUUID()
+function getUuid() {
+  // eslint-disable-next-line no-restricted-globals
+  return self.crypto.randomUUID()
 }
 
-function getElementByUuid (selector, uuid) {
-    const elements = Array.from(document.querySelectorAll(selector))
-    return elements.filter((item) => item.dataset.taskUuid === uuid)[0]
+function getElementByUuid(selector, uuid) {
+  const elements = Array.from(document.querySelectorAll(selector))
+  return elements.filter((item) => item.dataset.taskUuid === uuid)[0]
 }
 
-function findCompleteTasks () {
-    const tasks = JSON.parse(localStorage.getItem('tasks'))
-    let completeUuidTasks = []
-    Object.keys(tasks).map(key => {
-        if (tasks[key]['completeStatus']) {
-            completeUuidTasks.push(key)
-        }
-    })
+function findCompleteTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+  const completeUuidTasks = []
+  Object.keys(tasks).forEach((key) => {
+    if (tasks[key].completeStatus) {
+      completeUuidTasks.push(key)
+    }
+  })
 
-    let taskListItemTexts = Array.from(document.querySelectorAll('.task_list__item-text'))
+  const taskListItemTexts = Array.from(document.querySelectorAll('.task_list__item-text'))
 
-    // taskListItemTexts = taskListItemTexts.filter(item => completeUuidTasks.includes(item.dataset.taskUuid))
-    taskListItemTexts.map(item => {
-        if (completeUuidTasks.includes(item.dataset.taskUuid)) {
-            item.style.textDecoration = 'line-through'
-        } else {
-            item.style.textDecoration = 'none'
-        }
-    })
-}
-
-function moveDownCompleteTasks () {
-    let taskListItem = Array.from(document.querySelectorAll('.task_list__item'))
-    const tasks = JSON.parse(localStorage.getItem('tasks'))
-    let completeUuidTasks = []
-    Object.keys(tasks).map(key => {
-        if (tasks[key]['completeStatus']) {
-            completeUuidTasks.push(key)
-        }
-    })
-
-    // taskListItem = taskListItem.filter(item => completeUuidTasks.includes(item.dataset.taskUuid))
-    taskListItem.map(item => {
-        if (completeUuidTasks.includes(item.dataset.taskUuid)) {
-            const completedTask = tasks[item.dataset.taskUuid]
-            delete tasks[item.dataset.taskUuid]
-            tasks[item.dataset.taskUuid] = completedTask
-
-            
-            const taskItem = getElementByUuid('.task_list__item', item.dataset.taskUuid)
-            let parent = taskItem.parentNode
-            parent.appendChild(taskItem)
-        }
-    })
-
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-
-
-}
-
-function completeTask () {
-    const uuid = this.dataset.taskUuid
-    const tasks = JSON.parse(localStorage.getItem('tasks'))
-
-    let taskListItemText = getElementByUuid('.task_list__item-text', uuid)
-    
-
-    tasks[uuid]['completeStatus'] = !tasks[uuid]['completeStatus']
-
-    if (tasks[uuid]['completeStatus']) {
-        taskListItemText.style.textDecoration = 'line-through'
-        const completedTask = tasks[uuid]
-        delete tasks[uuid]
-        tasks[uuid] = completedTask
-        
-        const taskItem = getElementByUuid('.task_list__item', uuid)
-        let parent = taskItem.parentNode
-        parent.appendChild(taskItem)
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-
+  taskListItemTexts.forEach((item) => {
+    const taskItem = item
+    if (completeUuidTasks.includes(taskItem.dataset.taskUuid)) {
+      taskItem.style.textDecoration = 'line-through'
     } else {
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-        taskListItemText.style.textDecoration = 'none'
-        moveDownCompleteTasks()
+      taskItem.style.textDecoration = 'none'
     }
-
-    selectElementsByParity('evenElementsStatus', 'even')
-    selectElementsByParity('oddElementsStatus', 'odd')
+  })
 }
 
-function editTask () {
-    const uuid = this.dataset.taskUuid
-    
-    const tasks = JSON.parse(localStorage.getItem('tasks'))
-    const taskItem = getElementByUuid('.task_list__item', uuid)
-    const taskListItemText = getElementByUuid('.task_list__item-text', uuid)
-
-    taskListItemText.removeAttribute('readonly')
-    taskListItemText.focus()
-    taskListItemText.setSelectionRange(taskListItemText.value.length, taskListItemText.value.length)
-    taskListItemText.style.color = '#146189'
-
-    function onFocus () {
-        if (!taskListItemText.value) {
-            delete tasks[uuid]
-            localStorage.setItem('tasks', JSON.stringify(tasks))
-            taskItem.remove()
-            selectElementsByParity('evenElementsStatus', 'even')
-            selectElementsByParity('oddElementsStatus', 'odd')
-            return
-        }
-        taskListItemText.setAttribute('readonly', '')
-        taskListItemText.style.color = '#F2F2F2'
-        tasks[uuid]['title'] = taskListItemText.value
-        localStorage.setItem('tasks', JSON.stringify(tasks))
+function moveDownCompleteTasks() {
+  const taskListItem = Array.from(document.querySelectorAll('.task_list__item'))
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+  const completeUuidTasks = []
+  Object.keys(tasks).forEach((key) => {
+    if (tasks[key].completeStatus) {
+      completeUuidTasks.push(key)
     }
+  })
 
-    
-    taskListItemText.addEventListener('blur', function() {
-        onFocus.call(this)
-    })
-    
-    taskListItemText.addEventListener('keydown', function(e) {
-        if (e.key === "Enter") {
-            onFocus.call(this)
-        }
-    })
+  taskListItem.forEach((item) => {
+    if (completeUuidTasks.includes(item.dataset.taskUuid)) {
+      const completedTask = tasks[item.dataset.taskUuid]
+      delete tasks[item.dataset.taskUuid]
+      tasks[item.dataset.taskUuid] = completedTask
+      const taskItem = getElementByUuid('.task_list__item', item.dataset.taskUuid)
+      const parent = taskItem.parentNode
+      parent.appendChild(taskItem)
+    }
+  })
 
+  localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-function deleteTask () {
-    const uuid = this.dataset.taskUuid
+function selectElementsByParity(parityStatus, className) {
+  const parityElementsStatus = JSON.parse(localStorage.getItem(parityStatus))
 
-    let taskListItem = getElementByUuid('.task_list__item', uuid)
+  const taskListItems = Array.from(document.querySelectorAll('.task_list__item'))
 
-    const tasks = JSON.parse(localStorage.getItem('tasks'))
+  for (let i = 0; i < taskListItems.length; i += 1) {
+    taskListItems[i].classList.remove(className)
+    if (parityElementsStatus && className === 'odd') {
+      if (i % 2 === 0) {
+        taskListItems[i].classList.add(className)
+      }
+    } else if (parityElementsStatus && className === 'even') {
+      if (i % 2 !== 0) {
+        taskListItems[i].classList.add(className)
+      }
+    }
+  }
+}
 
+function completeTask() {
+  const uuid = this.dataset.taskUuid
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+  const taskListItemText = getElementByUuid('.task_list__item-text', uuid)
+  tasks[uuid].completeStatus = !tasks[uuid].completeStatus
+
+  if (tasks[uuid].completeStatus) {
+    taskListItemText.style.textDecoration = 'line-through'
+    const completedTask = tasks[uuid]
     delete tasks[uuid]
+    tasks[uuid] = completedTask
 
+    const taskItem = getElementByUuid('.task_list__item', uuid)
+    const parent = taskItem.parentNode
+    parent.appendChild(taskItem)
     localStorage.setItem('tasks', JSON.stringify(tasks))
+  } else {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    taskListItemText.style.textDecoration = 'none'
+    moveDownCompleteTasks()
+  }
 
-    taskListItem.remove()
-    selectElementsByParity('evenElementsStatus', 'even')
-    selectElementsByParity('oddElementsStatus', 'odd')
+  selectElementsByParity('evenElementsStatus', 'even')
+  selectElementsByParity('oddElementsStatus', 'odd')
 }
 
-function addListenersToActionBtns (selector, callback) {
-    const btns = Array.from(document.querySelectorAll(selector))
+function editTask() {
+  const uuid = this.dataset.taskUuid
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+  const taskItem = getElementByUuid('.task_list__item', uuid)
+  const taskListItemText = getElementByUuid('.task_list__item-text', uuid)
+  taskListItemText.removeAttribute('readonly')
+  taskListItemText.focus()
+  taskListItemText.setSelectionRange(taskListItemText.value.length, taskListItemText.value.length)
+  taskListItemText.style.color = '#146189'
 
-    btns.forEach((btn, i) => {
-        btn.removeEventListener('click', callback)
-        btn.addEventListener('click', callback)
-    })
-}
-
-function appendTask () {
-    const taskList = document.querySelector('.task-list')
-    const taskInput = document.querySelector('#task-input')
-
-    const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {}
-
-    const uuid = getUuid()
-
-    tasks[uuid] = {
-        title: taskInput.value,
-        completeStatus: false
+  function onFocus() {
+    if (!taskListItemText.value) {
+      delete tasks[uuid]
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+      taskItem.remove()
+      selectElementsByParity('evenElementsStatus', 'even')
+      selectElementsByParity('oddElementsStatus', 'odd')
+      return
     }
+    taskListItemText.setAttribute('readonly', '')
+    taskListItemText.style.color = '#F2F2F2'
+    tasks[uuid].title = taskListItemText.value
     localStorage.setItem('tasks', JSON.stringify(tasks))
-    taskList.innerHTML += getTaskListItem(uuid, taskInput.value)
-    taskInput.value = ''
-    
-    addListenersToActionBtns('.task_list__delete', deleteTask)
-    addListenersToActionBtns('.task_list__edit', editTask)
-    addListenersToActionBtns('.task_list__complete', completeTask)
+  }
+
+  taskListItemText.addEventListener('blur', function cb() {
+    onFocus.call(this)
+  })
+
+  taskListItemText.addEventListener('keydown', function cb(e) {
+    if (e.key === 'Enter') {
+      onFocus.call(this)
+    }
+  })
+}
+
+function deleteTask() {
+  const uuid = this.dataset.taskUuid
+
+  const taskListItem = getElementByUuid('.task_list__item', uuid)
+
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+
+  delete tasks[uuid]
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+  taskListItem.remove()
+  selectElementsByParity('evenElementsStatus', 'even')
+  selectElementsByParity('oddElementsStatus', 'odd')
+}
+
+function addListenersToActionBtns(selector, callback) {
+  const btns = Array.from(document.querySelectorAll(selector))
+
+  btns.forEach((btn) => {
+    btn.removeEventListener('click', callback)
+    btn.addEventListener('click', callback)
+  })
+}
+
+function appendTask() {
+  const taskList = document.querySelector('.task-list')
+  const taskInput = document.querySelector('#task-input')
+
+  const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {}
+
+  const uuid = getUuid()
+
+  tasks[uuid] = {
+    title: taskInput.value,
+    completeStatus: false,
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+  taskList.innerHTML += getTaskListItem(uuid, taskInput.value)
+  taskInput.value = ''
+
+  addListenersToActionBtns('.task_list__delete', deleteTask)
+  addListenersToActionBtns('.task_list__edit', editTask)
+  addListenersToActionBtns('.task_list__complete', completeTask)
+
+  moveDownCompleteTasks()
+
+  selectElementsByParity('evenElementsStatus', 'even')
+  selectElementsByParity('oddElementsStatus', 'odd')
+}
+
+function showTasks() {
+  const taskList = document.querySelector('.task-list')
+
+  const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {}
+
+  let taskItems = ''
+
+  Object.keys(tasks).forEach((uuid) => {
+    taskItems += getTaskListItem(uuid, tasks[uuid].title)
+  })
+
+  taskList.innerHTML = taskItems
+  addListenersToActionBtns('.task_list__delete', deleteTask)
+  addListenersToActionBtns('.task_list__edit', editTask)
+  addListenersToActionBtns('.task_list__complete', completeTask)
+
+  if (Object.keys(tasks).length > 0) {
+    findCompleteTasks()
 
     moveDownCompleteTasks()
 
     selectElementsByParity('evenElementsStatus', 'even')
     selectElementsByParity('oddElementsStatus', 'odd')
+  }
 }
 
-function showTasks () {
-    const taskList = document.querySelector('.task-list')
+function addListenersToSelectElementsBtns(btn, parityStatus, className) {
+  btn.addEventListener('click', () => {
+    if (localStorage.getItem(parityStatus) === null) {
+      localStorage.setItem(parityStatus, JSON.stringify(false))
+    }
+    const parityElementsStatus = !JSON.parse(localStorage.getItem(parityStatus))
+    localStorage.setItem(parityStatus, parityElementsStatus)
+    selectElementsByParity(parityStatus, className)
+  })
+}
 
+function addListenersForLastFirstElemDelBtns(btn, position) {
+  btn.addEventListener('click', () => {
     const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {}
 
-    let taskItems = ''
-    
-    Object.keys(tasks).map(uuid => {
-        taskItems += getTaskListItem(uuid, tasks[uuid]['title'])
-    })
-
-    taskList.innerHTML = taskItems
-    addListenersToActionBtns('.task_list__delete', deleteTask)
-    addListenersToActionBtns('.task_list__edit', editTask)
-    addListenersToActionBtns('.task_list__complete', completeTask)
+    const taskUuid = position === 'first' ? Object.keys(tasks)[0] : Object.keys(tasks)[Object.keys(tasks).length - 1]
 
     if (Object.keys(tasks).length > 0) {
-        findCompleteTasks()
-
-        moveDownCompleteTasks() //
-
-        selectElementsByParity('evenElementsStatus', 'even')
-        selectElementsByParity('oddElementsStatus', 'odd')
+      const data = {
+        dataset: {
+          taskUuid,
+        },
+      }
+      deleteTask.call(data)
     }
+  })
 }
-
-function selectElementsByParity (parityStatus, className) {
-    const parityElementsStatus = JSON.parse(localStorage.getItem(parityStatus))
-
-    const taskListItems = Array.from(document.querySelectorAll('.task_list__item'))
-
-    for (let i = 0; i < taskListItems.length; i++) {
-        taskListItems[i].classList.remove(className)
-        if (parityElementsStatus && className === 'odd') {
-            if (i % 2 === 0) {
-                taskListItems[i].classList.add(className)
-            }
-        } else if (parityElementsStatus && className === 'even') {
-            if (i % 2 !== 0) {
-                taskListItems[i].classList.add(className)
-            }
-        }
-    }
-
-}
-
-function addListenersToSelectElementsBtns (btn, parityStatus, className) {
-    btn.addEventListener('click', () => {
-        if (localStorage.getItem(parityStatus) === null) {
-            localStorage.setItem(parityStatus, JSON.stringify(false))
-        }
-        const parityElementsStatus = !JSON.parse(localStorage.getItem(parityStatus))
-        localStorage.setItem(parityStatus, parityElementsStatus)
-        selectElementsByParity(parityStatus, className)
-
-    })
-}
-
-function addListenersForLastFirstElemDelBtns (btn, position) {
-    btn.addEventListener('click', () => {
-
-        const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {}
-
-        const taskUuid = position === 'first' ? Object.keys(tasks)[0] : Object.keys(tasks)[Object.keys(tasks).length - 1]
-
-        if (Object.keys(tasks).length > 0) {
-            const data = {
-                'dataset': {
-                    'taskUuid': taskUuid
-                }
-            }
-            deleteTask.call(data)
-        }
-    })
-}
-
-///////////////////////////////////////////
 
 window.addEventListener('load', () => {
-    const taskInput = document.querySelector('#task-input')
-    const taskSubmit = document.querySelector('#task-submit')
-    const selectOddElementBtn = document.querySelector('.menu__item-btn-odd')
-    const selectEvenElementBtn = document.querySelector('.menu__item-btn-even')
-    const deleteFirstTaskElement = document.querySelector('.menu__item-btn-del-first')
-    const deleteLastTaskElement = document.querySelector('.menu__item-btn-del-last')
+  const taskInput = document.querySelector('#task-input')
+  const taskSubmit = document.querySelector('#task-submit')
+  const selectOddElementBtn = document.querySelector('.menu__item-btn-odd')
+  const selectEvenElementBtn = document.querySelector('.menu__item-btn-even')
+  const deleteFirstTaskElement = document.querySelector('.menu__item-btn-del-first')
+  const deleteLastTaskElement = document.querySelector('.menu__item-btn-del-last')
 
-    const menuBtn = document.querySelector('.menu-btn')
-    const menu = document.querySelector('.menu')
-    menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('active')
-        menu.classList.toggle('active')
-    })
+  const menuBtn = document.querySelector('.menu-btn')
+  const menu = document.querySelector('.menu')
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active')
+    menu.classList.toggle('active')
+  })
 
-    showTasks()
+  showTasks()
 
-    taskSubmit.addEventListener('click', e => {
+  taskSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (!taskInput.value) {
+      // alert('Укажите задачу')
+      return
+    }
+    appendTask()
+  })
 
-        e.preventDefault()
-        if (!taskInput.value) {
-            // alert('Укажите задачу')
-            return
-        }
-        appendTask()
-    })
-
-    addListenersToSelectElementsBtns(selectEvenElementBtn, 'evenElementsStatus', 'even')
-    addListenersToSelectElementsBtns(selectOddElementBtn, 'oddElementsStatus', 'odd')
-    addListenersForLastFirstElemDelBtns(deleteFirstTaskElement, 'first')
-    addListenersForLastFirstElemDelBtns(deleteLastTaskElement, 'last')
-
+  addListenersToSelectElementsBtns(selectEvenElementBtn, 'evenElementsStatus', 'even')
+  addListenersToSelectElementsBtns(selectOddElementBtn, 'oddElementsStatus', 'odd')
+  addListenersForLastFirstElemDelBtns(deleteFirstTaskElement, 'first')
+  addListenersForLastFirstElemDelBtns(deleteLastTaskElement, 'last')
 })
